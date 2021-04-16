0b06e9c11f87bd7839f0d341ed142ecea08e4fd9
Grace <jordanleo8803@gmail.com>
2021-04-16 12:42:12 -0400

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by grace Jordan on 4/16/21.
+//  Copyright © 2021 grace Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+var whoIs = "talking in class"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print (whoIs)
+    }
+
+
+}
+
